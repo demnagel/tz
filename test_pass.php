272f902492bd0b410df49cc4a5463c9b5b481555
demnagel <35008815+demnagel@users.noreply.github.com>
2019-06-18 22:13:39 +0300

    Create test_pass.php

---
+++ test_pass.php
@@ -0,0 +1,27 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+require 'body_class.php';
+require 'pass_class.php';
+
+class PassTest extends TestCase 
+{	
+    public function testPassTrue()
+    {
+        $pass = new tz\Pass(30, ['input' => true]);
+		$pass_inp_and_out = new tz\Pass(30, ['input' => true, 'output' => true]);
+	
+        $this->assertTrue($pass->controlUser(new tz\Body('input', 40))); 
+        $this->assertTrue($pass->controlUser(new tz\Body('input', 30))); 
+        $this->assertTrue($pass_inp_and_out->controlUser(new tz\Body('output', 30)));	
+	}
+	
+	public function testPassFalse()
+    {
+        $pass = new tz\Pass(30, ['input' => true]);
+		
+        $this->assertFalse($pass->controlUser(new tz\Body('input', 20))); 
+        $this->assertFalse($pass->controlUser(new tz\Body('output', 50))); 
+        $this->assertFalse($pass->controlUser(new tz\Body('eee', 50)));  
+	}
+}

4919c8d25f19358888e98facf6121dda47cb1553
demnagel <35008815+demnagel@users.noreply.github.com>
2019-06-18 22:18:45 +0300

    Update test_pass.php

--- test_pass.php
+++ test_pass.php
@@ -9,14 +9,14 @@ class PassTest extends TestCase
     public function testPassTrue()
     {
         $pass = new tz\Pass(30, ['input' => true]);
-		$pass_inp_and_out = new tz\Pass(30, ['input' => true, 'output' => true]);
+	$pass_inp_and_out = new tz\Pass(30, ['input' => true, 'output' => true]);
 	
         $this->assertTrue($pass->controlUser(new tz\Body('input', 40))); 
         $this->assertTrue($pass->controlUser(new tz\Body('input', 30))); 
         $this->assertTrue($pass_inp_and_out->controlUser(new tz\Body('output', 30)));	
 	}
 	
-	public function testPassFalse()
+    public function testPassFalse()
     {
         $pass = new tz\Pass(30, ['input' => true]);
 		
